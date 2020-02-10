3d37d50c111931564e5481756dcb9a7190d06381
Thiago Vieira de Aguiar <56936784+thiago-aguiar@users.noreply.github.com>
2020-02-10 11:48:34 -0300

    Update MainActivity.kt
    Teste de Pull.

--- app/src/main/java/com/example/exemplogit/MainActivity.kt
+++ app/src/main/java/com/example/exemplogit/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    
+    var novaVariavel : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
